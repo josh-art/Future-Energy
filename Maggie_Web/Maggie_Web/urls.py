b5a61fceb1fa36d65acb2ba6240d451425299cd0
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-18 09:00:12 +0300

    'Changes'

--- Maggie_Web/Maggie_Web/urls.py
+++ Maggie_Web/Maggie_Web/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.conf.urls import url
-from django.views.static import serve
 from django.conf.urls.static import static
 from . import settings
 
@@ -27,16 +25,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('Maggie_app.urls')),
 
-                  #url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-                  #url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+              ]
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-#    urlpatterns += static(settings.STATIC_URL,
- #                        document_root=settings.STATIC_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
