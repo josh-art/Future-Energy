bbb8b5b2817cd287680739bc1660251baa2c6530
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-19 00:50:20 +0300

    Update build.sh

--- Maggie_Web/build.sh
+++ Maggie_Web/build.sh
@@ -3,7 +3,7 @@
 #build the project
 
 echo "building the project..."
-python3.9 -m pip install -r Future-Energy/blob/main/Maggie_Web/requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
