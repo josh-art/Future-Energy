39a67fed24d8e6d156452b8eb0d5356a605cc750
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-19 00:40:36 +0300

    Update build.sh

--- Maggie_Web/build.sh
+++ Maggie_Web/build.sh
@@ -3,7 +3,7 @@
 #build the project
 
 echo "building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
