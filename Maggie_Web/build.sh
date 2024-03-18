01958ce34f8040debffca26f99540a788f95f4c4
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-19 00:58:25 +0300

    Update build.sh

--- Maggie_Web/build.sh
+++ Maggie_Web/build.sh
@@ -1,7 +1,8 @@
 #/bin/bsh
 
 #build the project
-
+echo "building the project..."
+python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
@@ -9,5 +10,6 @@ python3.9 manage.py migrate
 
 echo "collect static"
 python3.9 manage.py collectstatic
+
 
 
