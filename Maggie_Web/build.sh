b0d653938cdb579bfdafa539d5ddfe33ba738933
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-19 11:38:03 +0300

    Update build.sh

--- Maggie_Web/build.sh
+++ Maggie_Web/build.sh
@@ -2,15 +2,7 @@
 
 #build the project
 echo "building the project..."
-python3.9 -m pip install -r asgiref==3.7.2
-Django==4.2.11
-django-crispy-forms==2.1
-pillow==10.2.0
-psycopg2-binary==2.9.9
-sqlparse==0.4.4
-typing_extensions==4.10.0
-tzdata==2024.1
-
+python3.9 -m pip install -r requirements.txt
 
 echo "make migrations..."
 python3.9 manage.py makemigrations 
