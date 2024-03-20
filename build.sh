695d7b2433dbc90bc462937497a012a9b65acfc9
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-20 15:22:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 
 python3 -m pip install -r requirements.txt
 
-python3 manage.py collectstatic
-
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+python3 manage.py collectstatic
+
+
 
 
 
