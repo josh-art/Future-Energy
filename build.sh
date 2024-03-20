d20d3df6cd5eaf1ad99f38f58cd188d41230f2e0
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-20 15:49:23 +0300

    'vercel'

--- build.sh
+++ build.sh
@@ -1,35 +1,16 @@
-<<<<<<< HEAD:build.sh
 #/bin/bsh
 
 #build the project
-set -o errexit
+echo "building the project..."
+python3.9 -m pip install -r requirements.txt
 
-
-pip install -r requirements.txt
-
-python3.9 manage.py collectstatic
-
+echo "make migrations..."
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 
+echo "collect static"
+python3.9 manage.py collectstatic
 
 
 
 
-=======
-#/bin/bsh
-
-#build the project
-
-python3 -m pip install -r requirements.txt
-
-python3 manage.py collectstatic
-
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-
-
-
-
->>>>>>> 1980a40f8339e4703f152007a89213799f485f94:Maggie_Web/build.sh
