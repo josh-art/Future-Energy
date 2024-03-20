ccb56bc68786eccac5630ea45c82f05669e71907
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-20 14:21:10 +0300

    'new'

---
+++ Maggie_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaggieAppConfig(AppConfig):
+    name = 'Maggie_app'
---
+++ build.sh
@@ -0,0 +1,17 @@
+#/bin/bsh
+
+#build the project
+set -o errexit
+
+
+pip install -r requirements.txt
+
+python3.9 manage.py collectstatic
+
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
+
+
+
+
