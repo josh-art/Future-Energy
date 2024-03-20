09c3c2807e24334182dc545a7a04b677e6df6aa8
josh-art <56514140+josh-art@users.noreply.github.com>
2024-03-20 17:27:14 +0300

    Update settings.py

--- Future_Energy/settings.py
+++ Future_Energy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rmyefp4$q=f&#asd98jfu+gzl$^t2ds3$ta5lo3d7)$l6yi&0g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['* ']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
